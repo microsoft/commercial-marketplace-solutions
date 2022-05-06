9a94d3504bd79a57179926b6b0a46080d764dda8
Dr. Christian Geuer-Pollmann <chgeuer@microsoft.com>
2022-05-06 21:01:52 +0200

    Update API versions, single space in Authorization header, UTC time, and using the marketplace token for marketplace calls.

--- vm/ps/Invoke-Meter.ps1
+++ vm/ps/Invoke-Meter.ps1
@@ -15,7 +15,7 @@ function Invoke-Meter() {
     )
 
     # Get subscription and resource group
-    $MetadataUrl = "http://169.254.169.254/metadata/instance?api-version=2019-06-01"
+    $MetadataUrl = "http://169.254.169.254/metadata/instance?api-version=2021-12-13"
     $Metadata = Invoke-RestMethod -Headers @{ 'Metadata' = 'true' } -Uri $MetadataUrl
     
     # Get system identity access token
@@ -24,23 +24,25 @@ function Invoke-Meter() {
     $AuthResponse = Invoke-RestMethod -Headers @{ "Metadata" = "true" } -Uri $ManagementTokenUrl 
     
     $Headers = @{}
-    $Headers.Add("Authorization", "$($AuthResponse.token_type) " + " " + "$($AuthResponse.access_token)")
+    $Headers.Add("Authorization", "$($AuthResponse.token_type) $($AuthResponse.access_token)")
 
     # Make sure the system identity has at least reader permission on the resource group through the deployment template
-    $ManagementUrl = "https://management.azure.com/subscriptions/" + $Metadata.compute.subscriptionId + "/resourceGroups/" + $Metadata.compute.resourceGroupName + "?api-version=2019-10-01"
+    $ManagementUrl = "https://management.azure.com/subscriptions/$($Metadata.compute.subscriptionId)/resourceGroups/$($Metadata.compute.resourceGroupName)?api-version=2019-10-01"
     $ResourceGroupInfo = Invoke-RestMethod -Headers $Headers -Uri $ManagementUrl 
     $ManagedAppId = $ResourceGroupInfo.managedBy
 
     # Get the token for calling the metering API
-    $MeteringApiTokenUrl = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=20e940b3-4c77-4b0b-9a53-9e16a1b010a7"
+    $MeteringApiResourceID="20e940b3-4c77-4b0b-9a53-9e16a1b010a7"
+    $MeteringApiTokenUrl = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=$($MeteringApiResourceID)"
     $AuthResponse = Invoke-RestMethod -Headers @{ "Metadata" = "true" } -Uri $MeteringApiTokenUrl 
+    $Headers.Add("Authorization", "$($AuthResponse.token_type) $($AuthResponse.access_token)")
 
     # Set to use TLS 1.2
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
 
     # Invoke meter for last 60m of transactions
-    $StartTime = (Get-Date).AddMinutes(-60).ToString("yyyy-MM-ddTHH:mm:ssZ")
-
+    $StartTime = (Get-Date).ToUniversalTime().AddMinutes(-60).ToString("yyyy-MM-ddTHH:mm:ssZ")
+    
     $Body = @{ 
         "resourceUri"        = $ManagedAppId
         "quantity"           = $Quantity 

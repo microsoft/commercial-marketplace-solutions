84ae4fe9b209a1218913ea91a82dad1e54ad9d29
Dr Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2023-01-30 17:32:26 +0100

    Fix (#18)
    Fixed dictionary initialization bug.

--- vm/ps/Invoke-Meter.ps1
+++ vm/ps/Invoke-Meter.ps1
@@ -34,7 +34,8 @@ function Invoke-Meter() {
     # Get the token for calling the metering API
     $MeteringApiResourceID="20e940b3-4c77-4b0b-9a53-9e16a1b010a7"
     $MeteringApiTokenUrl = "http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=$($MeteringApiResourceID)"
-    $AuthResponse = Invoke-RestMethod -Headers @{ "Metadata" = "true" } -Uri $MeteringApiTokenUrl 
+    $AuthResponse = Invoke-RestMethod -Headers @{ "Metadata" = "true" } -Uri $MeteringApiTokenUrl
+    $Headers = @{}
     $Headers.Add("Authorization", "$($AuthResponse.token_type) $($AuthResponse.access_token)")
 
     # Set to use TLS 1.2
